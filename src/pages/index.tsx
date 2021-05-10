948db4eeb56c6ba14f75cd31d3b024f9fd23cfca
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-05-09 21:12:33 -0300

    Formatação de string

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import { GetStaticProps } from 'next';
+import { format, parseISO} from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import { api } from '../services/api';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
 type Episode = {
   id: string;
   title: string;
   members: string;
+  published_at: string;
 }
 type HomeProps = {
   episodes: Array<Episode>
@@ -28,10 +33,24 @@ export const getStaticProps: GetStaticProps = async () =>{
       _order: 'desc'
     }
   });
+  //formatação de dados
 
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      titulo: episode.title, 
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description, 
+      url: episode.file.url
+    }
+  })
   return {
     props: {
-      episodes: data,
+      episodes: episodes,
     }, 
     revalidate: 60 * 60 * 8, //segundos, minutos, horas - a cada 8 horas 
   }
